245e641b2cc29d81bb47ca3fa98b9d9346e044f2
pidaisaca <167306596+pidaisaca@users.noreply.github.com>
2025-03-24 13:58:47 +0000

    Creación base
    En esta actualización se ha creado la pantalla de login además de crear la clase de notas.

---
+++ GestorDeNotas/src/Main/GUI.java
@@ -0,0 +1,23 @@
+package Main;
+
+import Login.Login;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Pidaisaca
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login run = new Login();
+        run.setVisible(true);
+    }
+    
+}
