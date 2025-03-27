899d997ab15beaac2fa0ed9118b0b1f3f1dc0552
pidaisaca <167306596+pidaisaca@users.noreply.github.com>
2025-03-27 15:21:00 +0000

    Gestión de usuarios
    Añadida la gestión de usuarios, guardar al usuario en .txt, y guardar las notas en su .txt. También se han hecho cambios en la estructura.

---
+++ Actividad_Evaluacion_UT5-main/GestorDeNotas/src/Main/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Controlador.validador;
+import Vista.Gestor;
+import Vista.Login;
+import Vista.CreadorUsuario;
+
+
+
+
+/**
+ *
+ * @author Daniel Ramos Montoya
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Vista.Login login = new Login();
+        login.setVisible(true);
+    }
+
+}
+
